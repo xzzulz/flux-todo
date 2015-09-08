a2bb9f0b7fa30ff6d2e2c37a10e11779f8c8f7e6
nzonbi <nzonbi@gmail.com>
2015-09-08 13:46:51 -0430

    Add app actions and action creators

--- app/js/src/actions/actions.js
+++ app/js/src/actions/actions.js
@@ -1,36 +1,63 @@
 /**
  * actions
+ * Contains action constants, and the actions wrapped in action
+ * creator functions.
  */
 
 import dispatcher from '../dispatcher/dispatcher.js';
 
 
+
 export var actionTypes = {
   TODO_CREATE: 'todo_create',
-  TODO_DONE: 'todo_done'
+  TODO_DONE: 'todo_done',
+  TODO_REMOVE: 'todo_remove',
+  TODO_REMOVE_ALL_COMPLETED: 'todo_remove_all_completed',
 }
 
-//export { actionTypes as actionTypes }
+
 
 var actions = {
 
   /**
+   * Creates a new todo item
    * @param  {string} text
    */
   createTodo: function(text) {
     dispatcher.dispatch({
-      actionType: actionTypes.TODO_CREATE,
+      type: actionTypes.TODO_CREATE,
       text: text
     });
   },
 
   /**
+   * Swaps todo/done status from a todo item
    * @param  {string} id
    */
   done: function(id) {
     dispatcher.dispatch({
-      actionType: actionTypes.TODO_DONE,
+      type: actionTypes.TODO_DONE,
       id: id
+    });
+  },
+
+  /**
+   * Removes a todo item from the list
+   * @param  {string} id
+   */
+  remove: function(id) {
+    dispatcher.dispatch({
+      type: actionTypes.TODO_REMOVE,
+      id: id
+    });
+  },
+
+  /**
+   * Removes all completed todos
+   */
+  removeAllCompleted: function() {
+    dispatcher.dispatch({
+      type: actionTypes.TODO_REMOVE_ALL_COMPLETED
     });
   },
 
